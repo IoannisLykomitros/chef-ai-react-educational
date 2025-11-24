e9f4700d63c322e9fe2db495b8c5e8a76ed902fa
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-11-24 13:14:03 +0200

    Created add ingredient form in main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,18 @@ import Header from './Header';
 
 const Main = () => {
     return (
-        <div>
-        </div>
+        <main>
+            <form className='add-ingredient-form'>
+                <input 
+                    type="text" 
+                    placeholder="e.g. oregano" 
+                    aria-label='Add Ingredient'
+                    name='ingredient'
+                />
+                <button>Add Ingredient</button>
+            </form>
+        </main>
     );
 }
 
+export default Main;
