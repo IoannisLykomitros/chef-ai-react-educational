fb86f8c2c3c754cd954add46c3fd7d9fdc6bb649
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-11-24 15:12:19 +0200

    added logic to display the recipe formatte with react markdown

--- src/ai.js
+++ src/ai.js
@@ -1,11 +1,11 @@
 import OpenAI from 'openai';
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. Don't forger to add a description of the recipe in the beginning of your response, above the title of the recipe.
 `
 
 const openai = new OpenAI({
-  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  apiKey: import.meta.env.VOPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 });
 
